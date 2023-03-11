18161fbda5354e2bd3c2fd57715cd51050fdc598
comet959 <1376723538@qq.com>
2023-03-12 02:08:23 +0800

    add some commets

--- src/start.s
+++ src/start.s
@@ -7,3 +7,4 @@ _start:
     ldr   x30       , =stack_top  //; 栈顶指针
     mov   sp        , x30         //; 传递
     bl    not_main                //; 跳转
+    b     .                       //; 永远不会运行到这里
