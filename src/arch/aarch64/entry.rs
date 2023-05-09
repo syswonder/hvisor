6cc60cf4f57de36d6dfe4194ed5c19fc324effcc
likey <lkeyue@163.com>
2023-05-09 23:34:35 +0800

    dev

--- src/arch/aarch64/entry.rs
+++ src/arch/aarch64/entry.rs
@@ -1,14 +1,28 @@
+use crate::consts::{HV_HEADER_PTR, PER_CPU_SIZE};
 use core::arch::global_asm; // 支持内联汇编
-
 pub unsafe extern "C" fn el2_entry() -> i32 {
     core::arch::asm!(
         "
+        mrs	x1, esr_el2      //  Exception Syndrome Register
+        lsr	x1, x1, #26      // EC, bits [31:26]
+        cmp	x1, #0x16           // hvc ec value
+        b.ne	.		/* not hvc */
+        b {enable_mmu}
         bl {entry}     
         eret
     ",
         entry = sym crate::entry,
+        sym enable_mmu,
         options(noreturn),
 
+    );
+}
+pub unsafe extern "C" fn enable_mmu() -> i32 {
+    core::arch::asm!(
+        "
+        
+    ",
+        options(noreturn),
     );
 }
 global_asm!(
@@ -23,19 +37,31 @@ pub unsafe extern "C" fn arch_entry() -> i32 {
 
         mov	x16, x0
 	    mov	x17, x30
+        /* skip for now
+        TODO:1 change header or config read step into a singe not naked func
+        *      2 just read them depend on offset         
+        adr     x0, HV_HEADER_PTR               //store header addr for get info from it
+        adrp	x1, __core_end                  //get page pool addr for calculate config addr 
+        ldrh	w2, [x0, #HEADER_MAX_CPUS]      //get cpu num
+        mov	x3, #PER_CPU_SIZE                   //get percpu size
+        
+        * sysconfig = pool + max_cpus * percpu_size
+        madd	x1, x2, x3, x1 //get config addr
+        */
         ldr	x13, =BASE_ADDRESS 
-        ldr	x12, =0x7fc00000    //config file hv.phy_mem
-        sub	x11, x12, x13
+        ldr	x12, =0x7fc00000                    //should be read from config file
+        sub	x11, x12, x13                       //x11= (el2 mmu)virt-phy offset 
         ldr	x1, =bootstrap_vectors
         virt2phys x1       
 
         /* choose opcode */
         mov	x0, 0
-        hvc	#0   //install bootstrap vec
+        hvc	#0                                  //install bootstrap vec
 
-        hvc	#0	/* bootstrap vectors enter EL2 at el2_entry */
-        mov	x30, x17 /* we go back to linux */
-        //mov x0, -22 //return EINVAL ?driver
+        hvc	#0	                                /* bootstrap vectors enter EL2 at el2_entry */
+        b .                                     //do not return here
+        mov	x30, x17                            /* we go back to linux */
+        //mov x0, -22                           
         ret
 
     ",
--- src/consts.rs
+++ src/consts.rs
@@ -1,10 +1,6 @@
-use crate::config::HvSystemConfig;
 use crate::header::HvHeader;
-use crate::memory::addr::{align_up, VirtAddr};
-use crate::percpu::PerCpu;
-
+use crate::memory::addr::VirtAddr;
 pub use crate::memory::PAGE_SIZE;
-
 /// Size of the hypervisor heap.
 pub const HV_HEAP_SIZE: usize = 32 * 1024 * 1024; // 32 MB
 
@@ -12,28 +8,13 @@ pub const HV_HEAP_SIZE: usize = 32 * 1024 * 1024; // 32 MB
 pub const PER_CPU_SIZE: usize = 512 * 1024; // 512 KB
 
 /// Start virtual address of the hypervisor memory.
-pub const HV_BASE: usize = 0xffff_ff00_0000_0000;
+pub const HV_BASE: usize = 0xffffc0200000;
 
 /// Pointer of the `HvHeader` structure.
 pub const HV_HEADER_PTR: *const HvHeader = __header_start as _;
 
 /// Pointer of the per-CPU data array.
-pub const PER_CPU_ARRAY_PTR: *mut PerCpu = __core_end as _;
-
-/// Pointer of the `HvSystemConfig` structure.
-pub fn hv_config_ptr() -> *const HvSystemConfig {
-    (PER_CPU_ARRAY_PTR as usize + HvHeader::get().max_cpus as usize * PER_CPU_SIZE) as _
-}
-
-/// Pointer of the free memory pool.
-pub fn free_memory_start() -> VirtAddr {
-    align_up(hv_config_ptr() as usize + HvSystemConfig::get().size())
-}
-
-/// End virtual address of the hypervisor memory.
-pub fn hv_end() -> VirtAddr {
-    HV_BASE + HvSystemConfig::get().hypervisor_memory.size as usize
-}
+pub const PER_CPU_ARRAY_PTR: *mut VirtAddr = __core_end as _;
 
 extern "C" {
     fn __header_start();
