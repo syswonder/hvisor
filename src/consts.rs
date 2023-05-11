0283ca0f70068b4b0f00d182a27077886c4e3cdc
likey <lkeyue@163.com>
2023-05-12 01:23:41 +0800

    set boot page table

--- src/consts.rs
+++ src/consts.rs
@@ -5,7 +5,7 @@ pub use crate::memory::PAGE_SIZE;
 pub const HV_HEAP_SIZE: usize = 32 * 1024 * 1024; // 32 MB
 
 /// Size of the per-CPU data (stack and other CPU-local data).
-pub const PER_CPU_SIZE: usize = 512 * 1024; // 512 KB
+pub const PER_CPU_SIZE: usize = 512; // 512 B
 
 /// Start virtual address of the hypervisor memory.
 pub const HV_BASE: usize = 0xffffc0200000;
