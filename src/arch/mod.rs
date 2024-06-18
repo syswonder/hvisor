e265492e0b07e4b0da9cac5119ea409162fea529
wheatfox <enkerewpo@hotmail.com>
2024-06-18 16:02:53 +0800

    create arch/loongarch64 and update makefile

--- src/arch/mod.rs
+++ src/arch/mod.rs
@@ -4,9 +4,15 @@ pub mod aarch64;
 #[cfg(target_arch = "riscv64")]
 pub mod riscv64;
 
-// 导出模块，以便外部使用
+#[cfg(target_arch = "loongarch64")]
+pub mod loongarch64;
+
+// export modules for external use
 #[cfg(target_arch = "aarch64")]
 pub use aarch64::*;
 
 #[cfg(target_arch = "riscv64")]
 pub use riscv64::*;
+
+#[cfg(target_arch = "loongarch64")]
+pub use loongarch64::*;
