f6f329277543cc42398beb6aa2b89037aaf13ba1
Inquisitor-201 <1365605830@qq.com>
2024-07-04 20:54:28 +0800

    imx8mp config done

--- src/platform/qemu_aarch64.rs
+++ src/platform/qemu_aarch64.rs
@@ -1,7 +1,7 @@
 use crate::{arch::zone::HvArchZoneConfig, config::*};
 
-pub const ROOT_ZONE_DTB_ADDR: usize = 0xb0000000;
-pub const ROOT_ZONE_ENTRY: usize = 0xa0000000;
+pub const ROOT_ZONE_DTB_ADDR: usize = 0xa0000000;
+pub const ROOT_ZONE_ENTRY: usize = 0xa0400000;
 pub const ROOT_ZONE_CPUS: u64 = (1 << 0) | (1 << 1);
 
 pub const ROOT_ZONE_MEMORY_REGIONS: [HvConfigMemoryRegion; 3] = [
@@ -33,31 +33,3 @@ pub const ROOT_ARCH_ZONE_CONFIG: HvArchZoneConfig = HvArchZoneConfig {
     gicr_base: 0x80a0000,
     gicr_size: 0xf60000,
 };
-
-pub fn platform_root_zone_config() -> HvZoneConfig {
-    // fill zero for memory regions and interrupts
-
-    let mut memory_regions = [HvConfigMemoryRegion {
-        mem_type: 0,
-        physical_start: 0,
-        virtual_start: 0,
-        size: 0,
-    }; CONFIG_MAX_MEMORY_REGIONS];
-
-    memory_regions[..ROOT_ZONE_MEMORY_REGIONS.len()].copy_from_slice(&ROOT_ZONE_MEMORY_REGIONS);
-
-    let mut interrupts = [0; CONFIG_MAX_INTERRUPTS];
-    interrupts[..ROOT_ZONE_IRQS.len()].copy_from_slice(&ROOT_ZONE_IRQS);
-
-    HvZoneConfig::new(
-        0,
-        ROOT_ZONE_CPUS,
-        ROOT_ZONE_MEMORY_REGIONS.len() as u32,
-        memory_regions,
-        ROOT_ZONE_IRQS.len() as u32,
-        interrupts,
-        ROOT_ZONE_ENTRY as u64,
-        ROOT_ZONE_DTB_ADDR as u64,
-        ROOT_ARCH_ZONE_CONFIG,
-    )
-}
