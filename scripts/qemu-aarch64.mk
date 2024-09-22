07e209f7b00a1015a72379b49f955e650c89408c
KouweiLee <15035660024@163.com>
2024-09-22 19:50:26 +0800

    fix qemu bug: cpu a53->a57

--- scripts/qemu-aarch64.mk
+++ scripts/qemu-aarch64.mk
@@ -14,7 +14,7 @@ QEMU_ARGS := -machine virt,secure=on,gic-version=3,virtualization=on
 
 # QEMU_ARGS += -d int
 
-QEMU_ARGS += -cpu cortex-a53
+QEMU_ARGS += -cpu cortex-a57
 QEMU_ARGS += -smp 4
 QEMU_ARGS += -m 3G
 QEMU_ARGS += -nographic
@@ -29,15 +29,15 @@ QEMU_ARGS += -device loader,file="$(zone0_dtb)",addr=0xa0000000,force-raw=on
 QEMU_ARGS += -drive if=none,file=$(FSIMG1),id=Xa003e000,format=raw
 QEMU_ARGS += -device virtio-blk-device,drive=Xa003e000,bus=virtio-mmio-bus.31
 
-QEMU_ARGS += -drive if=none,file=$(FSIMG2),id=Xa003c000,format=raw
-QEMU_ARGS += -device virtio-blk-device,drive=Xa003c000
+# QEMU_ARGS += -drive if=none,file=$(FSIMG2),id=Xa003c000,format=raw
+# QEMU_ARGS += -device virtio-blk-device,drive=Xa003c000
 
 # QEMU_ARGS += -netdev tap,id=Xa003a000,ifname=tap0,script=no,downscript=no
 # QEMU_ARGS += -device virtio-net-device,netdev=Xa003a000,mac=52:55:00:d1:55:01
 # QEMU_ARGS += -netdev user,id=n0,hostfwd=tcp::5555-:22 -device virtio-net-device,bus=virtio-mmio-bus.29,netdev=n0 
 
-QEMU_ARGS += -chardev pty,id=Xa0038000
-QEMU_ARGS += -device virtio-serial-device,bus=virtio-mmio-bus.28 -device virtconsole,chardev=Xa0038000
+# QEMU_ARGS += -chardev pty,id=Xa0038000
+# QEMU_ARGS += -device virtio-serial-device,bus=virtio-mmio-bus.28 -device virtconsole,chardev=Xa0038000
 
 # QEMU_ARGS += --fsdev local,id=Xa0036000,path=./9p/,security_model=none
 # QEMU_ARGS += -device virtio-9p-pci,fsdev=Xa0036000,mount_tag=kmod_mount
