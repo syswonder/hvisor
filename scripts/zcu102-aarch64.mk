e8df0ade87cb5cd2d068526f71988e9681194273
wheatfox <enkerewpo@hotmail.com>
2024-12-10 15:38:01 +0800

    support FIT image packing and qemu boot using FIT

--- scripts/zcu102-aarch64.mk
+++ scripts/zcu102-aarch64.mk
@@ -2,27 +2,7 @@
 # created on 2024.12.2, wheatfox(enkerewpo@hotmail.com)
 
 # according to petalinux-boot qemu
-# however we must use petalinux-boot qemu to use it because it need some background servers
-
-# qemu-system-aarch64 -M arm-generic-fdt
-# -serial mon:stdio -serial /dev/null -display none 
-# -device loader,file=system.dtb,addr=0x100000,force-raw=on
-# -device loader,file=u-boot.elf
-# -device loader,file=Image,addr=0x200000,force-raw=on
-# -device loader,file=rootfs.cpio.gz.u-boot,addr=0x4000000,force-raw=on
-# -device loader,file=bl31.elf,cpu-num=0
-# -global xlnx,zynqmp-boot.cpu-num=0
-# -global xlnx,zynqmp-boot.use-pmufw=true
-# -global xlnx,zynqmp-boot.drive=pmu-cfg
-# -blockdev node-name=pmu-cfg,filename=pmu-conf.bin,driver=file
-
-# -hw-dtb zynqmp-qemu-multiarch-arm.dtb # <- this argument is added by xilinx for their qemu port
-
-# -device loader,file=boot.scr,addr=0x20000000,force-raw=on
-# -gdb tcp:localhost:9000
-# -net nic -net nic -net nic -net nic,netdev=eth3 -netdev user,id=eth3,tftp=/tftpboot
-# -machine-path /tmp/tmpbf8tgt6q
-# -m 4G
+# however we must use petalinux-boot qemu to use it because it need some background servers ?
 
 $(hvisor_bin): elf
 	@if ! command -v mkimage > /dev/null; then \
@@ -39,21 +19,20 @@ $(hvisor_bin): elf
 	fi && \
 	$(OBJCOPY) $(hvisor_elf) --strip-all -O binary $(hvisor_bin).tmp && \
 	mkimage -n hvisor_img -A arm64 -O linux -C none -T kernel -a 0x40400000 \
-	-e 0x40400000 -d $(hvisor_bin).tmp $(hvisor_bin) && \
-	rm -rf $(hvisor_bin).tmp
-
+	-e 0x40400000 -d $(hvisor_bin).tmp $(hvisor_bin)
 
 HVISOR_BIN_FULL_PATH = $(shell readlink -f $(hvisor_bin))
 DEFAULT_PETALINUX_PROJECT_PATH = /home/wheatfox/Documents/Code/petalinux_projects/wheatfox_hw0
 DEFAULT_PETALINUX_SDK_PATH = /home/wheatfox/petalinux_sdk
 
-FAKE_DTB = $(PETALINUX_PROJECT_PATH)/images/linux/system.dtb
-FAKE_DTB_PATH = $(shell readlink -f $(FAKE_DTB))
+ROOT_LINUX_IMAGE = $(PETALINUX_PROJECT_PATH)/images/linux/Image
+ROOT_LINUX_ROOTFS = $(PETALINUX_PROJECT_PATH)/images/linux/rootfs.cpio.gz.u-boot
+ROOT_LINUX_DTB = $(PETALINUX_PROJECT_PATH)/images/linux/system.dtb
 
-# args passed to xilinx's qemu
-EXTRA_QEMU_ARGS =
-EXTRA_QEMU_ARGS += -device loader,file=$(HVISOR_BIN_FULL_PATH),addr=0x40400000,force-raw=on
-EXTRA_QEMU_ARGS += -device loader,file=$(FAKE_DTB_PATH),addr=0x100000,force-raw=on
+TARGET_FIT_IMAGE = fitImage
+TARGET_FIT_IMAGE_PATH = $(shell readlink -f $(TARGET_FIT_IMAGE))
+
+GDB ?= aarch64-linux-gnu-gdb
 
 ifndef PETALINUX_SDK_ROOT
 	PETALINUX_SDK_ROOT = $(DEFAULT_PETALINUX_SDK_PATH)
@@ -70,8 +49,31 @@ else
 	PETALINUX_PROJECT_PATH = $(PETALINUX_PROJECT_PATH)
 endif
 
-.PHONY: run-petalinux-qemu
-run-petalinux-qemu: $(hvisor_bin)
+HVISOR_TMP_PATH = $(shell readlink -f $(hvisor_bin).tmp)
+
+.PHONY: gen-fit
+gen-fit:
+	@if [ ! -f scripts/zcu102-aarch64-fit.its ]; then \
+		echo "Error: ITS file scripts/zcu102-aarch64-fit.its not found."; \
+		exit 1; \
+	fi
+	$(OBJCOPY) $(hvisor_elf) --strip-all -O binary $(HVISOR_TMP_PATH)
+	@sed \
+		-e "s|__ROOT_LINUX_IMAGE__|$(ROOT_LINUX_IMAGE)|g" \
+		-e "s|__ROOT_LINUX_ROOTFS__|$(ROOT_LINUX_ROOTFS)|g" \
+		-e "s|__ROOT_LINUX_DTB__|$(ROOT_LINUX_DTB)|g" \
+		-e "s|__HVISOR_TMP_PATH__|$(HVISOR_TMP_PATH)|g" \
+		scripts/zcu102-aarch64-fit.its > temp-fit.its
+	@mkimage -f temp-fit.its $(TARGET_FIT_IMAGE)
+	@echo "Generated FIT image: $(TARGET_FIT_IMAGE)"
+
+# "pl" is short for "petalinux"
+# args passed to xilinx's qemu
+EXTRA_QEMU_ARGS =
+EXTRA_QEMU_ARGS += -device loader,file=$(TARGET_FIT_IMAGE_PATH),addr=0x50000000,force-raw=on
+
+.PHONY: run-pl-qemu
+run-pl-qemu: $(hvisor_bin) gen-fit
 	@echo "Running petalinux qemu..."
 # petalinux only works in bash
 # it will open a gdb server on tcp:localhost:9000
@@ -81,11 +83,17 @@ run-petalinux-qemu: $(hvisor_bin)
 		--qemu-args '$(EXTRA_QEMU_ARGS)' \
 		"
 
-GDB ?= aarch64-linux-gnu-gdb
+# uboot cmds:
+# bootm 0x50000000
 
-.PHONY: debug-petalinux-qemu
-debug-petalinux-qemu:
+.PHONY: debug-pl-qemu
+debug-pl-qemu:
 	@echo "Starting gdb client..."
 	$(GDB) -ex "target remote localhost:9000" -ex "layout asm"
 
-# manual uboot boot: bootm 0x40400000 - 0x100000
+# below are some quick commands to trigger petalinux commands
+.PHONY: pl-build
+pl-build:
+	@echo "Building petalinux project..."
+	bash -c "source $(PETALINUX_SDK_ROOT)/settings.sh && \
+		cd $(PETALINUX_PROJECT_PATH) && petalinux-build"
