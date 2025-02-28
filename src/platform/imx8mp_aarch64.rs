6221e8e76638ac153c5a110ec56bf6b55642c298
KouweiLee <15035660024@163.com>
2025-02-28 16:13:52 +0800

    fix issue: #109

--- src/platform/imx8mp_aarch64.rs
+++ src/platform/imx8mp_aarch64.rs
@@ -1,5 +1,5 @@
 use crate::{arch::zone::HvArchZoneConfig, config::*};
-// gpu on non root linux
+
 pub const ROOT_ZONE_DTB_ADDR: u64 = 0xa0000000;
 pub const ROOT_ZONE_KERNEL_ADDR: u64 = 0xa0400000;
 pub const ROOT_ZONE_ENTRY: u64 = 0xa0400000;
@@ -7,7 +7,7 @@ pub const ROOT_ZONE_CPUS: u64 = (1 << 0) | (1 << 1);
 
 pub const ROOT_ZONE_NAME: &str = "root-linux";
 
-pub const ROOT_ZONE_MEMORY_REGIONS: [HvConfigMemoryRegion; 3] = [
+pub const ROOT_ZONE_MEMORY_REGIONS: [HvConfigMemoryRegion; 8] = [
     HvConfigMemoryRegion {
         mem_type: MEM_TYPE_RAM,
         physical_start: 0x50000000,
@@ -22,28 +22,40 @@ pub const ROOT_ZONE_MEMORY_REGIONS: [HvConfigMemoryRegion; 3] = [
     }, // bus@30000000
     HvConfigMemoryRegion {
         mem_type: MEM_TYPE_IO,
+        physical_start: 0x30c00000,
+        virtual_start: 0x30c00000,
+        size: 0x400000,
+    }, 
+    HvConfigMemoryRegion {
+        mem_type: MEM_TYPE_IO,
         physical_start: 0x30800000,
         virtual_start: 0x30800000,
         size: 0x400000,
     }, 
-    // HvConfigMemoryRegion {
-    //     mem_type: MEM_TYPE_IO,
-    //     physical_start: 0x38000000,
-    //     virtual_start: 0x38000000,
-    //     size: 0x8000,
-    // },
-    // HvConfigMemoryRegion {
-    //     mem_type: MEM_TYPE_IO,
-    //     physical_start: 0x38008000,
-    //     virtual_start: 0x38008000,
-    //     size: 0x8000,
-    // },
-    // HvConfigMemoryRegion {
-    //     mem_type: MEM_TYPE_IO,
-    //     physical_start: 0x38500000,
-    //     virtual_start: 0x38500000,
-    //     size: 0x20000,
-    // }
+    HvConfigMemoryRegion {
+        mem_type: MEM_TYPE_IO,
+        physical_start: 0x38000000,
+        virtual_start: 0x38000000,
+        size: 0x8000,
+    },
+    HvConfigMemoryRegion {
+        mem_type: MEM_TYPE_IO,
+        physical_start: 0x38008000,
+        virtual_start: 0x38008000,
+        size: 0x8000,
+    },
+    HvConfigMemoryRegion {
+        mem_type: MEM_TYPE_IO,
+        physical_start: 0x38500000,
+        virtual_start: 0x38500000,
+        size: 0x20000,
+    },
+    HvConfigMemoryRegion {
+        mem_type: MEM_TYPE_IO,
+        physical_start: 0x32c00000,
+        virtual_start: 0x32c00000,
+        size: 0x400000,
+    }, // hdmi
     // bus@30800000
        // HvConfigMemoryRegion {
        //     mem_type: MEM_TYPE_IO,
@@ -53,8 +65,8 @@ pub const ROOT_ZONE_MEMORY_REGIONS: [HvConfigMemoryRegion; 3] = [
        // }, // serial
 ];
 
-pub const ROOT_ZONE_IRQS: [u32; 20] = [
-    36, 52, 55, 59, 64, 65, 67, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 150, 151, 152,
+pub const ROOT_ZONE_IRQS: [u32; 28] = [
+    35, 36, 37, 38, 45, 52, 55, 56, 57, 59, 64, 67, 75, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 135, 150, 151, 152, 162
 ];
 
 pub const ROOT_ARCH_ZONE_CONFIG: HvArchZoneConfig = HvArchZoneConfig {
@@ -62,19 +74,15 @@ pub const ROOT_ARCH_ZONE_CONFIG: HvArchZoneConfig = HvArchZoneConfig {
     gicd_size: 0x10000,
     gicr_base: 0x38880000,
     gicr_size: 0xc0000,
+    gicc_base: 0,
+    gicc_size: 0,
+    gicc_offset: 0x0,
+    gich_base: 0,
+    gich_size: 0,
+    gicv_base: 0,
+    gicv_size: 0,
     gits_base: 0,
     gits_size: 0,
 };
 
-pub const ROOT_ZONE_IVC_CONFIG: [HvIvcConfig; 1] = [
-    HvIvcConfig {
-        ivc_id: 0,
-        peer_id: 0,
-        control_table_ipa: 0xd000_0000,
-        shared_mem_ipa: 0xd000_1000,
-        rw_sec_size: 0,
-        out_sec_size: 0x1000,
-        interrupt_num: 0x21 + 32,
-        max_peers: 2,
-    }
-];
+pub const ROOT_ZONE_IVC_CONFIG: [HvIvcConfig; 0] = [];
