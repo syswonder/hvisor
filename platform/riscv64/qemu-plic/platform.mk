f23b2320771dea81e374c7604fd469385ac85500
刘景宇 <2537738252@qq.com>
2025-03-18 23:28:43 +0800

    riscv64: add aclint support, send_ipi

--- platform/riscv64/qemu-plic/platform.mk
+++ platform/riscv64/qemu-plic/platform.mk
@@ -10,7 +10,7 @@ zone0_dtb    := $(image_dir)/dts/zone0.dtb
 # zone1_kernel := $(image_dir)/kernel/Image
 # zone1_dtb    := $(image_dir)/devicetree/linux.dtb
 
-QEMU_ARGS := -machine virt
+QEMU_ARGS := -machine virt,aclint=on
 QEMU_ARGS += -bios default
 QEMU_ARGS += -cpu rv64
 QEMU_ARGS += -smp 4
