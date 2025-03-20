de48ea50866cbab9c6b0a7c41e0542adf5100286
wheatfox <enkerewpo@hotmail.com>
2025-03-20 17:29:56 +0800

    refactor: improve hvisor source tree detection in scripts

--- tools/clean.sh
+++ tools/clean.sh
@@ -3,12 +3,13 @@
 set -e
 
 function find_hvisor_src {
-    # get the directory which contains CHANGELOG.md and "hvisor" in the file contents
-    # this is the root of the hvisor source tree, return the absolute path
-    ret=$(find . -name CHANGELOG.md -exec grep -q hvisor {} \; -print -quit)
+    # find in ./CHANGELOG.md ../CHANGELOG.md
+    ret=$(find . -name CHANGELOG.md -exec grep -l "hvisor" {} \;)
     if [ -z "$ret" ]; then
-        echo "Could not find hvisor source tree"
-        exit 1
+        ret=$(find .. -name CHANGELOG.md -exec grep -l "hvisor" {} \;)
+    fi
+    if [ -z "$ret" ]; then
+        echo "."
     fi
     echo $(dirname $ret)
 }
--- tools/read_features.sh
+++ tools/read_features.sh
@@ -6,10 +6,7 @@ ARCH=$1
 BOARD=$2
 
 FEATURES="platform/$ARCH/$BOARD/cargo/features"
-# read the contents of the features file
 FEATURES=$(cat $FEATURES)
-# post process the features, replace any newlines with spaces
 FEATURES=$(echo $FEATURES | tr '\n' ' ')
 FEATURES=$(echo $FEATURES | tr -d '\r')
-# print the features
 echo $FEATURES
