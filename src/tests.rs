7a371de925256f2bfbdc01ef90274432c9b4e124
wheatfox <enkerewpo@hotmail.com>
2025-03-21 11:14:50 +0800

    license. update Mulan PSL v2 headers

--- src/tests.rs
+++ src/tests.rs
@@ -1,3 +1,18 @@
+// Copyright (c) 2025 Syswonder
+// hvisor is licensed under Mulan PSL v2.
+// You can use this software according to the terms and conditions of the Mulan PSL v2.
+// You may obtain a copy of Mulan PSL v2 at:
+//     http://license.coscl.org.cn/MulanPSL2
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR
+// FIT FOR A PARTICULAR PURPOSE.
+// See the Mulan PSL v2 for more details.
+//
+// Syswonder Website:
+//      https://www.syswonder.org
+//
+// Authors:
+//
 /// this module is for unittests of hvisor
 /// since this is a baremetal program
 /// all unittests are performed when running hvisor on qemu
