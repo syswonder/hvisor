40e8a621fcd2e0ee00e5a6349546aacd7fd5bf0c
MacixOwl <1043280679@qq.com>
2025-04-14 15:51:49 +0800

    Update auto_hvisor_qemu_gicv3.sh

--- tools/auto_hvisor_qemu_gicv3.sh
+++ tools/auto_hvisor_qemu_gicv3.sh
@@ -33,7 +33,7 @@ expect {
     }
 }
 
-
+# You can uncomment 'ntpdate cn.pool.ntp.org' to update the system time
 expect {
     "root@(none):/home/arm64# " {
         send "mount -t proc proc /proc \r\n\nmount -t sysfs sysfs /sys \r\n\nip link set eth0 up \r\n\ndhclient eth0 \r\n\nbrctl addbr br0 \r\n\nbrctl addif br0 eth0 \r\n\nifconfig eth0 0 \r\n\ndhclient br0 \r\n\nip tuntap add dev tap0 mode tap \r\n\nbrctl addif br0 tap0 \r\n\nip link set dev tap0 up \r\n\n #ntpdate cn.pool.ntp.org \r\n\n"
