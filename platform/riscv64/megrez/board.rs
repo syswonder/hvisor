ca740ddd783f582e95b53fa4ddd707b0ff532d78
刘景宇 <2537738252@qq.com>
2025-06-20 17:33:52 +0800

    riscv64: add platform megrez

---
+++ platform/riscv64/megrez/board.rs
@@ -0,0 +1,67 @@
+// Copyright (c) 2025 Syswonder
+// hvisor is licensed under Mulan PSL v2.
+// You can use this software according to the terms and conditions of the Mulan PSL v2.
+// You may obtain a copy of Mulan PSL v2 at:
+//     http://license.coscl.org.cn/MulanPSL2
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR
+// FIT FOR A PARTICULAR PURPOSE.
+// See the Mulan PSL v2 for more details.
+//
+// Syswonder Website:
+//      https://www.syswonder.org
+//
+// Authors:
+//
+use crate::{arch::zone::HvArchZoneConfig, config::*};
+
+#[allow(unused)]
+pub const BOARD_NAME: &str = "milkv-megrez";
+
+pub const BOARD_NCPUS: usize = 4;
+
+pub const PLIC_BASE: usize = 0xc000000;
+
+pub const BOARD_PLIC_INTERRUPTS_NUM: usize = 1023; // except irq 0
+
+pub const ROOT_ZONE_DTB_ADDR: u64 = 0x8f000000;
+pub const ROOT_ZONE_KERNEL_ADDR: u64 = 0x90000000;
+pub const ROOT_ZONE_ENTRY: u64 = 0x90000000;
+pub const ROOT_ZONE_CPUS: u64 = (1 << 0);
+
+pub const ROOT_ZONE_NAME: &str = "root-linux";
+
+pub const ROOT_ZONE_MEMORY_REGIONS: [HvConfigMemoryRegion; 3] = [
+    HvConfigMemoryRegion {
+        mem_type: MEM_TYPE_RAM,
+        physical_start: 0x8F000000,
+        virtual_start: 0x8F000000,
+        size: 0x71000000,
+    }, // ram
+    HvConfigMemoryRegion {
+        mem_type: MEM_TYPE_IO,
+        physical_start: 0x50900000,
+        virtual_start: 0x50900000,
+        size: 0x30000,
+    }, // serial
+    HvConfigMemoryRegion {
+        mem_type: MEM_TYPE_IO,
+        physical_start: 0x50470000,
+        virtual_start: 0x50470000,
+        size: 0x10000,
+    }, // mmc
+];
+
+// Note: all here's irqs are hardware irqs,
+//  only these irq can be transferred to the physical PLIC.
+pub const HW_IRQS: [u32; 3] = [100, 101, 102];
+
+// irqs belong to the root zone.
+pub const ROOT_ZONE_IRQS: [u32; 3] = [100, 101, 102];
+
+pub const ROOT_ARCH_ZONE_CONFIG: HvArchZoneConfig = HvArchZoneConfig {
+    plic_base: 0xc000000,
+    plic_size: 0x4000000,
+    aplic_base: 0xd000000,
+    aplic_size: 0x8000,
+};
