c3219e908b350396a71993d3b45ce9e79d246467
hangqi-ren <2572131118@qq.com>
2025-07-16 17:23:34 +0800

    feature: Remove gic version feature in arch/aarch64.

--- src/arch/aarch64/ipi.rs
+++ src/arch/aarch64/ipi.rs
@@ -14,37 +14,8 @@
 // Authors:
 //
 
-#[cfg(feature = "gicv3")]
-use crate::arch::sysreg::write_sysreg;
-#[cfg(feature = "gicv2")]
-use crate::device::irqchip::set_sgi_irq;
+use crate::device::irqchip::gic_send_event;
 pub fn arch_send_event(cpu_id: u64, sgi_num: u64) {
-    #[cfg(feature = "gicv3")]
-    {
-        /*Actually, the value passed to ICC_SGI1R_EL1 should be derived from
-        the MPIDR of the target CPU. However, since we cannot access this
-        register on the sender side, we have reverse-engineered a value
-        here using the cpu_id.
-        Due to differences in how some CPU implementations (e.g., RK3568 and RK3588)
-        encode affinity values in MPIDR, we use conditional compilation to handle
-        platform-specific mappings between cpu_id and interrupt target affinity.
-        */
-
-        use crate::arch::cpu::cpuid_to_mpidr_affinity;
-        let (aff3, aff2, aff1, aff0) = cpuid_to_mpidr_affinity(cpu_id);
-
-        let target_list = 1 << aff0;
-
-        let irm: u64 = 0 << 40;
-        let sgi_id: u64 = sgi_num << 24;
-        let val: u64 = aff1 | aff2 | aff3 | irm | sgi_id | target_list;
-        write_sysreg!(icc_sgi1r_el1, val);
-        debug!("write sgi sys value = {:#x}", val);
-    }
-    #[cfg(feature = "gicv2")]
-    {
-        let sgi_id: u64 = sgi_num;
-        let target_list: u64 = 1 << cpu_id;
-        set_sgi_irq(sgi_id as usize, target_list as usize, 0);
-    }
+    gic_send_event(cpu_id, sgi_num);
 }
+
