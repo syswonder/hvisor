e827145f42f2dd78064c4967061a8b1dac9d2d32
dallas <dallas.xyf@gmail.com>
2026-01-28 12:18:30 +0800

    Set the default serial port for screen to /dev/pts/0

--- platform/aarch64/qemu-gicv3/test/systemtest/boot_zone1.sh
+++ platform/aarch64/qemu-gicv3/test/systemtest/boot_zone1.sh
@@ -1,10 +1,10 @@
 # insmod hvisor.ko
 mkdir -p /dev/pts
 mount -t devpts devpts /dev/pts
-# exec rsyslog
-rsyslogd
+# # exec rsyslog
+# rsyslogd
 # start zone1
 ./hvisor virtio start zone1-linux-virtio.json &
 ./hvisor zone start zone1-linux.json && \
-grep "char device" /var/log/syslog && \
+# grep "char device" /var/log/syslog && \
 script /dev/null
--- platform/riscv64/qemu-plic/test/systemtest/boot_zone1.sh
+++ platform/riscv64/qemu-plic/test/systemtest/boot_zone1.sh
@@ -2,7 +2,7 @@
 rm nohup.out
 mkdir -p /dev/pts
 mount -t devpts devpts /dev/pts
-nohup ./hvisor virtio start virtio-backend.json &
+./hvisor virtio start virtio-backend.json &
 ./hvisor zone start zone1-linux-virtio.json && \
-cat nohup.out | grep "char device" && \
+# cat /var/log/syslog | grep "char device" && \
 script /dev/null
