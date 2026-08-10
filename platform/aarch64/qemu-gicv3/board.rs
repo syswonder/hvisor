0d3514d52488e5eb89f43c8da9fc547a5a1f92b4
dallas <dallas.xyf@gmail.com>
2026-08-10 22:44:13 +0800

    for qemu-gicv3 aarch64 remove cpu 2,3 in zone0

--- platform/aarch64/qemu-gicv3/board.rs
+++ platform/aarch64/qemu-gicv3/board.rs
@@ -53,7 +53,7 @@ pub const BOARD_PHYSMEM_LIST: &[(u64, u64, MemoryType)] = &[
 pub const ROOT_ZONE_DTB_ADDR: u64 = 0xa0000000;
 pub const ROOT_ZONE_KERNEL_ADDR: u64 = 0xa0400000;
 pub const ROOT_ZONE_ENTRY: u64 = 0xa0400000;
-pub const ROOT_ZONE_CPUS: u64 = (1 << 0) | (1 << 1) | (1 << 2) | (1 << 3);
+pub const ROOT_ZONE_CPUS: u64 = (1 << 0) | (1 << 1);
 
 pub const ROOT_ZONE_NAME: &str = "root-linux";
 
